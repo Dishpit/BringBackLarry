835a21203138bddefb2cb023e928685d610ad2a4
Zebulun McNeill <zeb.mcn@gmail.com>
2024-02-08 00:01:31 -0600

    target secondary post button, and logo

--- content.js
+++ content.js
@@ -1,12 +1,33 @@
-function changeButtonText() {
-    const postButton = document.querySelector('a[aria-label="Post"]');
-        if (postButton) {
-            const spans = postButton.querySelectorAll('span');
+function fixBranding() {
+    const primaryPostButton = document.querySelector('a[aria-label="Post"]');
+        if (primaryPostButton) {
+            const spans = primaryPostButton.querySelectorAll('span');
             const lastSpan = spans[spans.length - 1];
 
             if (lastSpan && lastSpan.textContent === 'Post') {
                 lastSpan.textContent = 'Tweet';
             }
         }
+
+    const secondaryPostButton = document.querySelector('div[data-testid="tweetButton"]');
+    if (secondaryPostButton) {
+        const spanTestId = secondaryPostButton.querySelector('span > span');
+        if (spanTestId && spanTestId.textContent.includes('Post')) {
+            spanTestId.textContent = 'Tweet';
+        }
+    }
+
+    const homeLink = document.querySelector('a[aria-label="X"]');
+    if (homeLink) {
+        const svgElement = homeLink.querySelector('svg');
+        if (svgElement) {
+            const Larry = document.createElement('img');
+            Larry.src = chrome.runtime.getURL('icon.png');
+            Larry.alt = 'Larry, the Twitter Bird';
+            Larry.style.width = '24px';
+            Larry.style.height = '24px';
+            svgElement.replaceWith(Larry);
+        }
+    }
 }
-setInterval(changeButtonText, 1000);
+setInterval(fixBranding, 1000);
